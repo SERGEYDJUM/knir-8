5cdee049bcc6849cb5e3decc4e554e4fa5651a52
SERGEYDJUM <39106181+SERGEYDJUM@users.noreply.github.com>
2025-04-10 13:01:12 +0300

    Refactor test module, add table output

--- test/test.py
+++ test/test.py
@@ -3,22 +3,45 @@ from cho import CHO, CHOss, CHOArray
 from model import CNNMO
 
 from sklearn.metrics import roc_auc_score
+from prettytable import PrettyTable, TableStyle
 from scipy.stats import spearmanr, pearsonr
 from numpy.typing import NDArray
 
 from os import path
+from dataclasses import dataclass
 
 import numpy as np
 import torch
 
 
 ROI_R: int = 32
-CNNMO_CP_PATH: str = "checkpoints/cnn_mo.pt"
-CNNMO_PIXEL_MUL = 0.001
+PIXEL_MUL = 0.001
 
-CHO_NOISE_MUL = 1.35
+CNNMO_CP_PATH: str = "checkpoints/cnn_mo.pt"
+
+CHO_NOISE_MUL = 1.1
 CHO_T_NOISE_STD = 1
 CHO_TRAIN_SET_PART = 1
+
+CHOSS_NOISE_MUL = 1.35
+CHOSS_T_NOISE_STD = 1
+CHOSS_TRAIN_SET_PART = 1
+
+CHOSS_ENABLED = True
+CHO_RESTRICTED = True
+
+MAIN_MODEL_NAME = "CNN-MO"
+ALT_MODEL_NAME = "CHOss" if CHOSS_ENABLED else "CHO"
+ALT_MODEL_NAME_EXT = "13xCHOss" if CHOSS_ENABLED else "13xCHO"
+ALT_MODEL_NAME_EXT += " (restricted)" if CHO_RESTRICTED else ""
+
+
+@dataclass
+class Results:
+    human_aucs: list[float]
+    main_aucs: list[float]
+    alt_aucs: list[float]
+    alt_auc_stds: list[float]
 
 
 class DataStore:
@@ -82,7 +105,7 @@ def measure_dist(
     model: CHO | CHOss | CHOArray,
     X: NDArray,
     y: NDArray,
-    n: int = 512,
+    n: int = 256,  # 512
     places: NDArray = None,
 ) -> tuple[float, float]:
     measurements = np.zeros(n, dtype=np.double)
@@ -98,60 +121,129 @@ def measure_dist(
 
 def measure_main(model: CNNMO, X: NDArray, y: NDArray) -> float:
     with torch.no_grad():
-        inp = torch.from_numpy(X * CNNMO_PIXEL_MUL)
-        inp = inp.reshape((X.shape[0], 1, X.shape[1], X.shape[2]))
+        inp = torch.from_numpy(X).reshape((X.shape[0], 1, X.shape[1], X.shape[2]))
         return roc_auc_score(y, model(inp).flatten())
 
 
-def print_corrs(human_aucs: list, main_aucs: list, alt_aucs: list) -> None:
-    main_sc_s = spearmanr(human_aucs, main_aucs)
-    alt_sc_s = spearmanr(human_aucs, alt_aucs)
+def calc_corrs(
+    human_aucs: list, main_aucs: list, alt_aucs: list
+) -> tuple[tuple, tuple]:
+    main_sc_p = pearsonr(human_aucs, main_aucs).statistic
+    alt_sc_p = pearsonr(human_aucs, alt_aucs).statistic
+    main_sc_s = spearmanr(human_aucs, main_aucs).statistic
+    alt_sc_s = spearmanr(human_aucs, alt_aucs).statistic
 
-    print(
-        f"\t\tSpearman r: Main={main_sc_s.statistic:.3f}, Alt={alt_sc_s.statistic:.3f}"
-    )
-
-    main_sc_p = pearsonr(human_aucs, main_aucs)
-    alt_sc_p = pearsonr(human_aucs, alt_aucs)
-
-    print(
-        f"\t\tPearson r: Main={main_sc_p.statistic:.3f}, Alt={alt_sc_p.statistic:.3f}"
-    )
+    return (main_sc_p, alt_sc_p), (main_sc_s, alt_sc_s)
 
 
-def adequacy_score(aucs: list[float], ref: list[float]) -> float:
-    assert len(aucs) == len(ref)
+def adequacy_score(aucs: list[float], ref: list[float]) -> tuple[float]:
+    n = len(aucs)
+    assert n == len(ref)
+    # mse = sum(map(lambda t: (t[0] - t[1]) ** 2, zip(aucs, ref))) / len(ref)
     pear_r = pearsonr(aucs, ref).statistic
-    mse = sum(map(lambda t: (t[0] - t[1]) ** 2, zip(aucs, ref))) / len(ref)
+    deltamu = abs(sum(aucs) / n - sum(ref) / n)
+    return ((pear_r + 1) / 2) * (1 - deltamu), deltamu
 
-    return ((pear_r + 1) / 2) * (1 - mse)
+
+def round_list(l: list[float]) -> list[str]:
+    return list(map(lambda x: f"{round(x, 3):.3f}", l))
 
 
-def main():
-    data = DataStore()
+def print_auc_table(
+    human_aucs: list[float],
+    main_aucs: list[float],
+    alt_aucs: list[float],
+    alt_auc_stds: list[float],
+    index_start: int = 1,
+) -> None:
+    table = PrettyTable()
+    table.set_style(TableStyle.MARKDOWN)
+    table.field_names = [
+        "CFG",
+        "Human",
+        "CNN-MO",
+        ALT_MODEL_NAME + " (μ)",
+        ALT_MODEL_NAME + " (σ)",
+    ]
+    table.add_rows(
+        list(
+            zip(
+                range(index_start, len(human_aucs) + index_start),
+                round_list(human_aucs),
+                round_list(main_aucs),
+                round_list(alt_aucs),
+                round_list(alt_auc_stds),
+            )
+        ),
+        divider=True,
+    )
 
-    kernel_soft = data.k
-    kernel_standard = np.logical_not(kernel_soft)
+    print(table)
 
-    small_objects = data.o
-    big_objects = np.logical_not(small_objects)
 
-    main_model = load_main_model()
+def print_res_table(
+    main_p: float,
+    alt_p: float,
+    main_s: float,
+    alt_s: float,
+    main_mse: float,
+    alt_mse: float,
+    main_adequacy: float,
+    alt_adequacy: float,
+) -> None:
+    table = PrettyTable()
+    table.set_style(TableStyle.MARKDOWN)
+    table.field_names = ["", "Spearman's ρ", "Pearson's ρ", "Δμ", "Adequacy"]
+    table.add_rows(
+        [
+            [
+                MAIN_MODEL_NAME,
+                f"{main_s:.3f}",
+                f"{main_p:.3f}",
+                f"{main_mse:.3f}",
+                f"__{main_adequacy:.3f}__",
+            ],
+            [
+                ALT_MODEL_NAME,
+                f"{alt_s:.3f}",
+                f"{alt_p:.3f}",
+                f"{alt_mse:.3f}",
+                f"__{alt_adequacy:.3f}__",
+            ],
+        ],
+        divider=True,
+    )
 
-    human_aucs = []
-    main_aucs = []
-    alt_aucs = []
-    alt_auc_stds = []
+    print(table)
 
-    def perform_experiment(
-        is_small_objects: bool, is_kernel_soft: bool, tube_current: int
+
+class ExperimentExecutor:
+    def __init__(self) -> None:
+        self.data = DataStore()
+        self.main_model = load_main_model()
+        self.results = Results([], [], [], [])
+
+        self.kernel_soft = self.data.k
+        self.small_objects = self.data.o
+        self.kernel_standard = np.logical_not(self.kernel_soft)
+        self.big_objects = np.logical_not(self.small_objects)
+        self.scored = self.data.hy != -1
+
+    def perform(
+        self, is_small_objects: bool, is_kernel_soft: bool, tube_current: int
     ) -> None:
-        current_f = data.a == tube_current
-        object_size_f = small_objects if is_small_objects else big_objects
-        kernel_f = kernel_soft if is_kernel_soft else kernel_standard
+        current_f = self.data.a == tube_current
+        object_size_f = self.small_objects if is_small_objects else self.big_objects
+        kernel_f = self.kernel_soft if is_kernel_soft else self.kernel_standard
 
         alt_model = CHOArray(
             CHOss(
+                channel_noise_std=CHOSS_NOISE_MUL,
+                test_stat_noise_std=CHOSS_T_NOISE_STD,
+                train_set_keep=CHOSS_TRAIN_SET_PART,
+            )
+            if CHOSS_ENABLED
+            else CHO(
                 channel_noise_std=CHO_NOISE_MUL,
                 test_stat_noise_std=CHO_T_NOISE_STD,
                 train_set_keep=CHO_TRAIN_SET_PART,
@@ -159,123 +251,139 @@ def main():
         )
 
         train_filter = np.logical_and(
-            kernel_standard,
+            self.kernel_standard if CHO_RESTRICTED else kernel_f,
             np.logical_and(current_f, object_size_f),
         )
 
         alt_model.train(
-            data.x[train_filter],
-            data.y[train_filter],
-            data.s[train_filter],
+            self.data.x[train_filter] * PIXEL_MUL,
+            self.data.y[train_filter],
+            self.data.s[train_filter],
         )
 
         ex_filter = np.logical_and(
-            np.logical_and(kernel_f, data.hy != -1),
+            np.logical_and(kernel_f, self.scored),
             np.logical_and(current_f, object_size_f),
         )
 
-        inp = data.x[ex_filter]
-        gt = data.y[ex_filter]
-        hy = data.hy[ex_filter]
-        places = data.s[ex_filter]
+        inp = self.data.x[ex_filter] * PIXEL_MUL
+        gt = self.data.y[ex_filter]
+        hy = self.data.hy[ex_filter]
+        places = self.data.s[ex_filter] if isinstance(alt_model, CHOArray) else None
 
         assert inp.shape[0] == gt.shape[0] == hy.shape[0]
 
         human_auc = roc_auc_score(gt, hy)
-        main_auc = measure_main(main_model, inp, gt)
-        alt_auc, alt_auc_std = measure_dist(
-            alt_model,
-            inp,
-            gt,
-            places=places if isinstance(alt_model, CHOArray) else None,
-        )
+        main_auc = measure_main(self.main_model, inp, gt)
+        alt_auc, alt_auc_std = measure_dist(alt_model, inp, gt, places=places)
 
-        print(
-            f"\t> Human={human_auc:.3f}, Main={main_auc:.3f}, Alt={alt_auc:.3f} (std_Alt={alt_auc_std:.3f})"
-        )
+        self.results.human_aucs.append(human_auc)
+        self.results.main_aucs.append(main_auc)
+        self.results.alt_aucs.append(alt_auc)
+        self.results.alt_auc_stds.append(alt_auc_std)
 
-        human_aucs.append(human_auc)
-        main_aucs.append(main_auc)
-        alt_aucs.append(alt_auc)
-        alt_auc_stds.append(alt_auc_std)
 
-    print("\nTrain set AUCs:")
+def main():
+    ex = ExperimentExecutor()
+
+    print(f"# {MAIN_MODEL_NAME} vs {ALT_MODEL_NAME_EXT}")
+    print("\n## Train set AUCs\n")
 
     # Configuration #1
-    perform_experiment(
+    ex.perform(
         is_small_objects=True,
         is_kernel_soft=False,
         tube_current=10,
     )
 
     # Configuration #2
-    perform_experiment(
+    ex.perform(
         is_small_objects=True,
         is_kernel_soft=False,
         tube_current=40,
     )
 
     # Configuration #3
-    perform_experiment(
+    ex.perform(
         is_small_objects=False,
         is_kernel_soft=False,
         tube_current=10,
     )
 
     # Configuration #4
-    perform_experiment(
+    ex.perform(
         is_small_objects=False,
         is_kernel_soft=False,
         tube_current=40,
     )
 
-    print("\n\tCorrelations:")
-    print_corrs(human_aucs, main_aucs, alt_aucs)
+    res = ex.results
 
-    ade_main = adequacy_score(main_aucs, human_aucs)
-    ade_alt = adequacy_score(alt_aucs, human_aucs)
-    print(f"\n\t=> Adequacy: Main={ade_main:.3f}, Alt={ade_alt:.3f}")
+    (main_p, alt_p), (main_s, alt_s) = calc_corrs(
+        res.human_aucs, res.main_aucs, res.alt_aucs
+    )
+    ade_main, main_mse = adequacy_score(res.main_aucs, res.human_aucs)
+    ade_alt, alt_mse = adequacy_score(res.alt_aucs, res.human_aucs)
 
-    print("\nTest set AUCs:")
+    print_auc_table(res.human_aucs, res.main_aucs, res.alt_aucs, res.alt_auc_stds)
+    print("\n### Metrics\n")
+    print_res_table(main_p, alt_p, main_s, alt_s, main_mse, alt_mse, ade_main, ade_alt)
+
+    print("\n## Test set AUCs\n")
 
     # Configuration #5
-    perform_experiment(
+    ex.perform(
         is_small_objects=True,
         is_kernel_soft=True,
         tube_current=10,
     )
 
     # Configuration #6
-    perform_experiment(
+    ex.perform(
         is_small_objects=True,
         is_kernel_soft=True,
         tube_current=40,
     )
 
     # Configuration #7
-    perform_experiment(
+    ex.perform(
         is_small_objects=False,
         is_kernel_soft=True,
         tube_current=10,
     )
 
     # Configuration #8
-    perform_experiment(
+    ex.perform(
         is_small_objects=False,
         is_kernel_soft=True,
         tube_current=40,
     )
 
-    print("\n\tCorrelations:")
-    print_corrs(human_aucs[4:], main_aucs[4:], alt_aucs[4:])
+    res = ex.results
 
-    ade_main = adequacy_score(main_aucs[4:], human_aucs[4:])
-    ade_alt = adequacy_score(alt_aucs[4:], human_aucs[4:])
-    print(f"\n\t=> Adequacy: Main={ade_main:.3f}, Alt={ade_alt:.3f}")
+    print_auc_table(
+        res.human_aucs[4:],
+        res.main_aucs[4:],
+        res.alt_aucs[4:],
+        res.alt_auc_stds[4:],
+        index_start=5,
+    )
 
-    print("\nEntire dataset:\n\t...")
-    print_corrs(human_aucs, main_aucs, alt_aucs)
+    (main_p, alt_p), (main_s, alt_s) = calc_corrs(
+        res.human_aucs[4:], res.main_aucs[4:], res.alt_aucs[4:]
+    )
+    ade_main, main_mse = adequacy_score(res.main_aucs[4:], res.human_aucs[4:])
+    ade_alt, alt_mse = adequacy_score(res.alt_aucs[4:], res.human_aucs[4:])
 
-    ade_main = adequacy_score(main_aucs, human_aucs)
-    ade_alt = adequacy_score(alt_aucs, human_aucs)
-    print(f"\n\t=> Adequacy: Main={ade_main:.3f}, Alt={ade_alt:.3f}")
+    print("\n### Metrics\n")
+    print_res_table(main_p, alt_p, main_s, alt_s, main_mse, alt_mse, ade_main, ade_alt)
+
+    print("\n## Dataset metrics\n")
+
+    (main_p, alt_p), (main_s, alt_s) = calc_corrs(
+        res.human_aucs, res.main_aucs, res.alt_aucs
+    )
+    ade_main, main_mse = adequacy_score(res.main_aucs, res.human_aucs)
+    ade_alt, alt_mse = adequacy_score(res.alt_aucs, res.human_aucs)
+
+    print_res_table(main_p, alt_p, main_s, alt_s, main_mse, alt_mse, ade_main, ade_alt)
